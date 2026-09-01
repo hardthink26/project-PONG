df6796972f6c62436b01790e028df006a3f035fd
soohyun <254634043+hardthink26@users.noreply.github.com>
2026-09-01 09:53:45 +0900

    refactor

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let height = canvas.height;
 let width = canvas.width;
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, width, height); 
 
 const BALL_SIZE = 5;
 let ballPosition = { x: 20, y: 30}; 
 
-ctx.fillStyle = "white";
-ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE); 
+function draw(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
+}
+
+draw(); 
+
 
 
 
