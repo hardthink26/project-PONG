2f414a74f1dbb6b9ee5903149fca5efc36e22344
soohyun <254634043+hardthink26@users.noreply.github.com>
2026-09-01 10:18:23 +0900

    move the ball

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let width = canvas.width;
 const BALL_SIZE = 5;
 let ballPosition = { x: 20, y: 30}; 
 
+let xSpeed = 4 
+let ySpeed = 2 
+
 function draw(){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, width, height);
@@ -14,7 +17,23 @@ function draw(){
     ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
 }
 
-draw(); 
+
+function update(){
+    ballPosition.x += xSpeed;
+    ballPosition.y += ySpeed;
+} 
+
+
+function gameLoop(){ 
+    draw();
+    update();
+
+    setTimeout(gameLoop, 30); 
+} 
+
+
+gameLoop(); 
+
 
 
 
